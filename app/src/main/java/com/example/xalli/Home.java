df17f2d25788ac096eed85b4f61434ee9f431564
Yael_Urquieta <byrurquieta@gmail.com>
2025-09-24 17:00:02 -0600

    Menu inferior y correciones en los colores

---
+++ app/src/main/java/com/example/xalli/Home.java
@@ -0,0 +1,14 @@
+package com.example.xalli;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class Home extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.home, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/xalli/Mapa.java
@@ -0,0 +1,12 @@
+package com.example.xalli;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Mapa {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.mapa, container, false);
+    }
+}
